5ea7f97baba7b36b2634e0a9ee2b750693f3cb0a
szymoninetum <szymon.laskowicz@inetum.com>
2022-03-23 10:30:18 +0100

    clean code

--- FIzzBuzz.sh
+++ FIzzBuzz.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
+
 for ((i=0; i < 100; i++))
-do
-if (( i % 15 == 0 )); then
-echo "FizzBuzz"
-elif (( i % 3 == 0 )); then
-echo "Fizz"
-elif (( i % 5 == 0 )); then
-echo "Buzz"
-else
-echo  "$i "
-fi
-done
+  do
+    if (( i % 15 == 0 )); then
+      echo "FizzBuzz"
+    elif (( i % 3 == 0 )); then
+      echo "Fizz"
+    elif (( i % 5 == 0 )); then
+      echo "Buzz"
+    else
+      echo  "$i "
+    fi
+  done
